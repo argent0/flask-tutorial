a41791603af66ffa50dc5d8a13ea72a3c35f5e93
Aner Lucero <alucero@blueboot.com>
2024-01-10 10:33:23 -0300

    Blog blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -37,5 +37,9 @@ def create_app(test_config : Optional[dict] = None) -> Flask:
     from . import auth
     app.register_blueprint(auth.bp)
 
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
 
--- flaskr/auth.py
+++ flaskr/auth.py
@@ -22,7 +22,7 @@ def register():
         if error is None:
             try:
                 db.execute(
-                        'INSERT INTO user (username, password) VALUES (?, ?)',
+                        'INSERT INTO users (username, password) VALUES (?, ?)',
                         (username, generate_password_hash(password))
                         )
                 db.commit()
@@ -39,11 +39,11 @@ def register():
 def login():
     if request.method == 'POST':
         username = request.form['username']
-        password = requesr.form['password']
+        password = request.form['password']
         db = get_db()
         error = None
-        user = db.execure(
-                'SELECT * FROM user WHERE username = ?', (username,)
+        user = db.execute(
+                'SELECT * FROM users WHERE username = ?', (username,)
                 ).fetchone()
 
         if user is None:
@@ -68,7 +68,7 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = get_db().execute(
-                'SELECT * FROM user WHERE id = ?', (user_id,)
+                'SELECT * FROM users WHERE id = ?', (user_id,)
                 ).fetchone()
 
 @bp.route('/logout')
